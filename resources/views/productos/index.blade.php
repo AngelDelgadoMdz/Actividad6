9567d40ca144a988ca3f12b6bc6e0de6972e1b80
Angel_Delgado <al02959951@tecmilenio.mx>
2023-03-02 20:27:07 -0500

    Agregar botón a la vista index de producto

--- resources/views/productos/index.blade.php
+++ resources/views/productos/index.blade.php
@@ -4,9 +4,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--Agregando boostrap-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
     <title>Vista del producto</title>
 </head>
 <body>
-    Página principal de productos
+    
+    <div class="container-fluid h-100">
+        <div class="row w-100">
+            <div class="col">
+                <button class="btn btn-danger center-block"> A regular button </button>
+            </div>
+        </div>
+    </div>
+
 </body>
 </html>
